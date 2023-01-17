76f4b7036b35acea99981ac023e7f0737b3abd01
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-17 20:21:51 +0530

    Add Register form and Login Form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
-import Home from './Pages/Home';
+import { Login } from "./Pages/Home/Login";
+import { Register } from "./Pages/Home/Register";
 
 function App() {
+    const [currentForm, setCurrentForm] = useState('login');
+
+    const toggleForm = (formName) => {
+        setCurrentForm(formName);
+    }
+
     return ( <
-        div className = "App" >
-        <
-        Home / >
-        <
+        div className = "App" > {
+            currentForm === "login" ? < Login onFormSwitch = { toggleForm }
+            /> : <Register onFormSwitch={toggleForm} / >
+        } <
         /div>
-    )
+    );
 }
 
 export default App;
